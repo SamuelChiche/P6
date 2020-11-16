953b2545b40c85b821c34d99557db7b92bf00ccc
Samuel Chiche <samuel.chiche24@gmail.com>
2020-11-16 11:01:03 +0100

    Sign up function with encrypt password added

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -4,7 +4,17 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
 exports.signup = (req, res, next) => {
-
+    bcrypt.hash(req.body.password, 12)
+        .then(hash => {
+            const user = new User({
+                email: req.body.email,
+                password: hash
+            });
+            user.save()
+                .then(() => res.status(201).json({ message: 'User created'}))
+                .catch(error => res.status(400).json({ error }));
+        })
+        .catch(error => res.status(400).json({ error }));
 };
 
 exports.login = (req, res, next) => {
