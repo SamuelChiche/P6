313243696cca6551e4924d5b461dd87159e2c1e1
Samuel Chiche <samuel.chiche24@gmail.com>
2020-11-16 07:45:52 +0100

    Preparing controllers

---
+++ backend/controllers/sauces.js
@@ -0,0 +1,22 @@
+const Sauce = require('../models/Sauce');
+const fs = require('fs');
+
+exports.createSauce = (req, res, next) => {
+
+};
+
+exports.modifySauce = (req, res, next) => {
+
+};
+
+exports.deleteSauce = (req, res, next) => {
+
+};
+
+exports.getAllSauces = (req, res, next) => {
+
+};
+
+exports.getOneSauce = (req, res, next) => {
+
+};
