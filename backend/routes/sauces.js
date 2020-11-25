f39a9d366d724f24de4cdb13d50804f5f4fe2d12
Samuel Chiche <samuel.chiche24@gmail.com>
2020-11-25 15:40:26 +0100

    Sauces routes updated with auth middleware

--- backend/routes/sauces.js
+++ backend/routes/sauces.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
+const auth = require('../middleware/auth');
+
 const saucesCtrl = require('../controllers/sauces');
 
 const multer = require('../middleware/multer-config');
 
-router.get('/', saucesCtrl.getAllSauces);
-router.post('/', multer, saucesCtrl.createSauce);
-router.get('/:id', saucesCtrl.getOneSauce);
-router.put('/:id', saucesCtrl.modifySauce);
-router.delete('/:id', saucesCtrl.deleteSauce);
+router.get('/', auth, saucesCtrl.getAllSauces);
+router.post('/', auth, multer, saucesCtrl.createSauce);
+router.get('/:id', auth, saucesCtrl.getOneSauce);
+router.put('/:id', auth, multer, saucesCtrl.modifySauce);
+router.delete('/:id', auth, saucesCtrl.deleteSauce);
+router.post('/:id/like', auth, saucesCtrl.likeSauce);
 
 
 module.exports = router;
